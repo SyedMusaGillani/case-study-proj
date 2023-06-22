d7e2ee75cf91e4389d9a4925c3e7ac393b65c52b
musa gillani <musa.gillani@devsinc.com>
2023-06-22 20:09:41 +0500

    added logic to calculate skus from demand_history with planning levels for last 30 days of orders

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const db = require("./database/models/index");
 
 const Task1 = async () => {
-  const data = await db.demand_history.findAll({
-    limit: 30,
+  const date = await db.demand_history.findAll({
+    attributes: ["order_placed_datetime"],
+    limit: 1,
+    order: [["order_placed_datetime", "DESC"]],
     raw: true,
     nested: true,
-    include: [{ model: db.sku_mapping, attributes: { exclude: ["sku"] } }],
+    include: [{ model: db.sku_mapping, attributes: [] }],
   });
-  console.log("data", data);
+  const specificDatetime = new Date(date[0].order_placed_datetime);
+
+  const data = await db.demand_history.findAll({
+    where: {
+      order_placed_datetime: {
+        [db.Sequelize.Op.gte]: db.Sequelize.literal(
+          `TIMESTAMP '${specificDatetime.toISOString()}' - INTERVAL '30 days'`
+        ),
+      },
+    },
+    raw: true,
+    nested: true,
+    include: [
+      {
+        model: db.sku_mapping,
+        attributes: { exclude: ["sku"] },
+        required: true,
+      },
+    ],
+  });
+  console.table(data);
+  console.log("total number of records", data.length);
 };
 
 async function main() {
